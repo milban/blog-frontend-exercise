98280b13e3e322001c10ffe78ccdd4692674eaa8
JunHyeock Park <wnsgur6311@gmail.com>
2020-11-06 07:20:10 +0900

    Implement Header component

---
+++ src/components/common/Header.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import styled from 'styled-components';
+import Responsive from 'components/common/Responsive';
+import Button from 'components/common/Button';
+
+const HeaderBlock = styled.header`
+  position: fixed;
+  width: 100%;
+  background: white;
+  box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.08);
+`;
+
+const Wrapper = styled(Responsive)`
+  height: 4rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  .logo {
+    font-size: 1.125rem;
+    font-weight: 800;
+    letter-spacing: 2px;
+  }
+  .right {
+    display: flex;
+    align-items: center;
+  }
+`;
+
+const Spacer = styled.div`
+  height: 4rem;
+`;
+
+const Header: React.FC = () => {
+  return (
+    <>
+      <HeaderBlock>
+        <Wrapper>
+          <div className="logo">MILBANS</div>
+          <div className="right">
+            <Button>로그인</Button>
+          </div>
+        </Wrapper>
+      </HeaderBlock>
+      <Spacer />
+    </>
+  );
+};
+
+export default Header;

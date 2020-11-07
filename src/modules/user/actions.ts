6eb90a026463ac6372e13bccff6f91a9a373200e
JunHyeock Park <wnsgur6311@gmail.com>
2020-11-07 16:58:28 +0900

    Handle login failures

--- src/modules/user/actions.ts
+++ src/modules/user/actions.ts
@@ -1,4 +1,10 @@
-import { CHECK, Check, TEMP_SET_USER, TempSetUser } from 'modules/user/types';
+import {
+  CHECK,
+  Check,
+  CHECK_FAILURE,
+  TEMP_SET_USER,
+  TempSetUser,
+} from 'modules/user/types';
 import createRequestSaga from 'lib/createRequestSaga';
 import * as authApi from 'lib/api/auth';
 import { takeLatest } from 'redux-saga/effects';
@@ -14,6 +20,14 @@ export const check: Check = () => ({
 });
 
 const checkSaga = createRequestSaga(CHECK, authApi.check);
+const checkFailureSaga = () => {
+  try {
+    localStorage.removeItem('user');
+  } catch (e) {
+    console.log('localStorage is not working');
+  }
+};
 export function* userSaga() {
   yield takeLatest(CHECK, checkSaga);
+  yield takeLatest(CHECK_FAILURE, checkFailureSaga);
 }
